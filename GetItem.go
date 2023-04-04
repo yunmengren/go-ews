dd0839b0b2d93756675ecfe3efa92c1da0f57eef
donghan <donghan@threatbook.cn>
2023-04-04 17:23:24 +0800

    fix: GetItem argument error

--- GetItem.go
+++ GetItem.go
@@ -8,7 +8,7 @@ import (
 // Find information about the FindItem EWS operation.
 // The FindItem operation searches for items that are located in a user's mailbox. This operation provides many ways to filter and format how search results are returned to the caller.
 // https://learn.microsoft.com/en-us/exchange/client-developer/web-service-reference/finditem-operation
-func (c *client) GetItem(eItem *elements.FindItem) (*elements.GetItemResponse, error) {
+func (c *client) GetItem(eItem *elements.GetItem) (*elements.GetItemResponse, error) {
 	oem, err := operations.NewEnvelopeMarshal(eItem)
 	if err != nil {
 		return nil, err
